3b6c0fb3843c0ae730d3422c1e509ac8b62de28c
Isaac Ortiz Muñoz <isaac.ortiz.mx@schilleramericas.com>
2021-05-06 10:24:45 -0500

    :zap: Update: Design System

--- src/app/components/login/login.tsx
+++ src/app/components/login/login.tsx
@@ -3,25 +3,21 @@ import { EmailInputComponent } from 'devteam-components-library';
 
 function Login() {
     return (
-        <div>
-            <div>
-                <EmailInputComponent
-                    idInput=''
-                    labelInput=''
-                    nameInput='email'
-                    textInput='email'
-                    typeInput='text'
-                />
-            </div>
-            <div>
-                <EmailInputComponent
-                    idInput=''
-                    labelInput=''
-                    nameInput='password'
-                    textInput='password'
-                    typeInput='password'
-                />
-            </div>
+        <div className="Login ColumnSE">
+            <EmailInputComponent
+                idInput=''
+                labelInput=''
+                nameInput='email'
+                textInput='email'
+                typeInput='text'
+            />
+            <EmailInputComponent
+                idInput=''
+                labelInput=''
+                nameInput='password'
+                textInput='password'
+                typeInput='password'
+            />
         </div>
     );
 }
